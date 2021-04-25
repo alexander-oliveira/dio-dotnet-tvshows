c409e6581bab86f8e350d40a7b6c7f109e678b1b
Alexander <alex_osilva@hotmail.com>
2021-04-25 20:58:13 -0300

    feat(Serie): add Serie properties

--- classes/Serie.cs
+++ classes/Serie.cs
@@ -4,6 +4,11 @@ namespace DIO.Series
 {
     public class Serie : EntidadeBase
     {
-        
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
     }    
 }
