3642b9dee5cca9c5240177831fac389a18a749b5
Alexander <alex_osilva@hotmail.com>
2021-04-26 16:33:46 -0300

    refactor(SerieTest):  change mock object scope

--- test/SerieTest.cs
+++ test/SerieTest.cs
@@ -1,21 +1,23 @@
 using System;
 using Xunit;
 using DIO.Series.Class;
+using DIO.Series.Enum;
 
 namespace DIO.Series.Test
 {
     public class SerieTest
     {
+        const int SerieId = 0;
+        const Genero SerieGenero = Genero.Acao;
+        const String SerieTitulo = "Serie ficticia";
+        const int SerieAno = 2021;
+        const string SerieDescricao = "Descricao ficticia";
+        Serie Instancia;
+        
         [Fact]
         public void Serie_Instanciacao_Sucesso()
         {
         //Given
-            const int SerieId = 0;
-            const Genero SerieGenero = Genero.Acao;
-            const String SerieTitulo = "Serie ficticia";
-            const int SerieAno = 2021;
-            const string SerieDescricao = "Descricao ficticia";
-            Serie Instancia;
         //When
             Instancia = new Serie(id: SerieId, genero: SerieGenero, titulo: SerieTitulo, ano: SerieAno, descricao: SerieDescricao);
         //Then
